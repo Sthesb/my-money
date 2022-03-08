9acd61370d5a375e88299967861ef432be9681cd
Sthembiso Mathebula <Sthembiso.Mathebula@gijima.com>
2022-03-08 16:56:58 +0200

    Total amount

--- src/Pages/Home/TransactionsList.js
+++ src/Pages/Home/TransactionsList.js
@@ -5,16 +5,28 @@ import styles from './Home.module.css'
 import { useFireStore } from '../../Hooks/useFireStore'
 
 export default function TransactionsList({ transactions }) {
+  const amounts = []
   const { deleteDocument } = useFireStore('transactions');
+  transactions.map(transact => (
+    amounts.push(parseInt(transact.amount))
+  ))
+  var sum = amounts.reduce(function(a, b){
+    return a + b;
+  }, 0);
+
   return (
-    <ul className={styles.transactions}>
+    <>
+      <h4>Total amount spent:</h4>
+      <h2>R {sum}</h2>
+      <ul className={styles.transactions}>
         {transactions.map((transaction) => (
             <li key={transaction.id}> 
-                <p  className={styles.name}>{transaction.name}</p>
-                <p className={styles.amount}>R {transaction.amount}</p>
-                <button onClick={() => deleteDocument(transaction.id) }>x</button>
+                  <p  className={styles.name}>{transaction.name}</p>
+                  <p className={styles.amount}>R {transaction.amount}</p>
+                  <button onClick={() => deleteDocument(transaction.id) }>x</button>
             </li>
         ))}
-    </ul>
+      </ul>
+    </>
   )
 }
